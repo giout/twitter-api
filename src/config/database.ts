2ada66078ce066bd5de66411b4cc55747b29f9af
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-10-30 16:50:25 -0400

    sequelize descartado

--- src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { Sequelize } from 'sequelize'
-
-const db = <string> process.env.DB_URI
-
-const database = new Sequelize(db)
-
-export default database
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import 'dotenv/config'
 import app from './config/app'
-import database from './config/database'
 
 const port = process.env.PORT || 4000
-
-database.authenticate().then(() => console.log('Connected to db'))
 
 app.listen(port, () => {console.log('Listening on port', port)})
