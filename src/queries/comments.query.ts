a1fd6198b11c4725ec3a0971c747b3852004ca23
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-11 16:51:57 -0400

    datos de usuario agregados a consultas select para comentarios y tweets

--- src/queries/comments.query.ts
+++ src/queries/comments.query.ts
@@ -1,17 +1,21 @@
+const data = `u.alias, u.first_name, u.last_name, p.*`
+const joinPostUser = `posts as p INNER JOIN users as u ON p.user_id=u.user_id`
+const isComment = `comment_to IS NOT NULL`
+
 const sql = {
     selectAllBy: {
-        user: 'SELECT * FROM posts WHERE user_id=$1 AND comment_to IS NOT NULL ORDER BY creation_date DESC',
-        pk: 'SELECT * FROM posts WHERE post_id=$1 AND comment_to IS NOT NULL',
-        tweet: 'SELECT * FROM posts WHERE comment_to=$1 ORDER BY creation_date DESC OFFSET $2 LIMIT $3'
+        user: `SELECT ${data} FROM ${joinPostUser} WHERE user_id=$1 AND ${isComment} ORDER BY creation_date DESC`,
+        pk: `SELECT ${data} FROM ${joinPostUser} WHERE post_id=$1 AND ${isComment}`,
+        tweet: `SELECT ${data} FROM ${joinPostUser} WHERE comment_to=$1 ORDER BY creation_date DESC OFFSET $2 LIMIT $3`
     },
 
-    insert: 'INSERT INTO posts (user_id, post_content, comment_to) VALUES ($1, $2, $3) RETURNING *',
+    insert: `INSERT INTO posts (user_id, post_content, comment_to) VALUES ($1, $2, $3) RETURNING ${data}`,
 
     update: {
-        content: 'UPDATE posts SET post_content=$1 WHERE post_id=$2'
+        content: `UPDATE posts SET post_content=$1 WHERE post_id=$2`
     }, 
     
-    delete: 'DELETE FROM posts WHERE post_id=$1'
+    delete: `DELETE FROM posts WHERE post_id=$1`
 }
 
 export default sql
--- src/queries/tweets.query.ts
+++ src/queries/tweets.query.ts
@@ -1,28 +1,32 @@
+const data = `u.alias, u.first_name, u.last_name, p.*`
+const joinPostUser = 'posts as p INNER JOIN users as u ON p.user_id=u.user_id'
+const isTweet = 'comment_to IS NULL'
+
 const sql = {
     selectAll: {
-        lessPopular: 'SELECT * FROM posts WHERE comment_to IS NULL ORDER BY post_likes ASC OFFSET $1 LIMIT $2',
-        popular: 'SELECT * FROM posts WHERE comment_to IS NULL ORDER BY post_likes DESC OFFSET $1 LIMIT $2',
-        oldest: 'SELECT * FROM posts WHERE comment_to IS NULL ORDER BY creation_date ASC OFFSET $1 LIMIT $2',
-        new: 'SELECT * FROM posts WHERE comment_to IS NULL ORDER BY creation_date DESC OFFSET $1 LIMIT $2'
+        lessPopular: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} ORDER BY post_likes ASC OFFSET $1 LIMIT $2`,
+        popular: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} ORDER BY post_likes DESC OFFSET $1 LIMIT $2`,
+        oldest: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} ORDER BY creation_date ASC OFFSET $1 LIMIT $2`,
+        new: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} ORDER BY creation_date DESC OFFSET $1 LIMIT $2`
     },
 
     selectAllBy: {
         user: {
-            lessPopular: 'SELECT * FROM posts WHERE comment_to IS NULL AND user_id=$1 ORDER BY post_likes ASC OFFSET $2 LIMIT $3',
-            popular: 'SELECT * FROM posts WHERE comment_to IS NULL AND user_id=$1 ORDER BY post_likes DESC OFFSET $2 LIMIT $3',
-            oldest: 'SELECT * FROM posts WHERE comment_to IS NULL AND user_id=$1 ORDER BY creation_date ASC OFFSET $2 LIMIT $3',
-            new: 'SELECT * FROM posts WHERE comment_to IS NULL AND user_id=$1 ORDER BY creation_date DESC OFFSET $2 LIMIT $3'            
+            lessPopular: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} AND user_id=$1 ORDER BY post_likes ASC OFFSET $2 LIMIT $3`,
+            popular: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} AND user_id=$1 ORDER BY post_likes DESC OFFSET $2 LIMIT $3`,
+            oldest: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} AND user_id=$1 ORDER BY creation_date ASC OFFSET $2 LIMIT $3`,
+            new: `SELECT ${data} FROM ${joinPostUser} WHERE ${isTweet} AND user_id=$1 ORDER BY creation_date DESC OFFSET $2 LIMIT $3`            
         },
-        pk: 'SELECT * FROM posts WHERE post_id=$1 AND comment_to IS NULL'
+        pk: `SELECT ${data} FROM ${joinPostUser} WHERE post_id=$1 AND ${isTweet}`
     },
 
-    insert: 'INSERT INTO posts (user_id, post_content) VALUES ($1, $2) RETURNING *',
+    insert: `INSERT INTO posts (user_id, post_content) VALUES ($1, $2) RETURNING ${data}`,
 
     update: {
-        content: 'UPDATE posts SET post_content=$1 WHERE post_id=$2'
+        content: `UPDATE posts SET post_content=$1 WHERE post_id=$2`
     }, 
     
-    delete: 'DELETE FROM posts WHERE post_id=$1'
+    delete: `DELETE FROM posts WHERE post_id=$1`
 }
 
 export default sql
