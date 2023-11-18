bbcb470e586dec3afb56d19f210cfe29a4f27331
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-18 09:20:09 -0400

    validacion de clave al editar usuario agregada

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -7,6 +7,8 @@ import { userExists, userIsAuth, verifyFollow } from "../utils/users"
 import { findFollowersByPk, findFollowingsByPk } from "../services/users.service"
 import { setLikes } from "../utils/posts"
 import { findCommentsByUser } from "../services/comments.service"
+import { validatePassword } from "../utils/validation"
+import CustomError from "../utils/CustomError"
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -64,8 +66,12 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
 
         const { password } = req.body
 
-        if (password)
+        if (password) {
+            if (!validatePassword(password)) {
+                throw new CustomError('Password must contain at least 8 characters, letters and numbers.', 400)
+            }
             req.body.password = encrypt(password)
+        }
     
         const updatedUser = await updateUserByPk(id, req.body)
         res.status(200).json(updatedUser)
